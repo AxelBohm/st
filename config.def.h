3c8721458c012e307d62b513d44869d00198ae34
Paul <ping.paul@hotmail.com>
2018-10-05 15:57:42 +0200

    change boarder pixel

--- config.def.h
+++ config.def.h
@@ -5,8 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Mono:pixelsize=13:antialias=true:autohint=true";
-static int borderpx = 10;
+static char *font = "Mono:pixelsize=12:antialias=true:autohint=true";
+static int borderpx = 2;
 
 /*
  * What program is execed by st depends of these precedence rules:
