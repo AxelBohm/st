27b2f9a354d3117a6ae3f5cdb18d985c7614e6cb
AxelBohm <axel.boehm@ymail.com>
2018-12-17 09:04:28 +0100

    new shortcuts for scrolling up and down

--- config.h
+++ config.h
@@ -182,6 +182,8 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_I,           iso14755,       {.i =  0} },
 	{ ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
 	{ ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
+	{ TERMMOD,              XK_K,           kscrollup,      {.i = -1} },
+	{ TERMMOD,              XK_J,           kscrolldown,    {.i = -1} },
 };
 
 /*
