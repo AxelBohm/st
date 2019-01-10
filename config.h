33c253d59311c084cc8d7ebb55e368da97304f28
AxelBohm <axel.boehm@ymail.com>
2019-01-10 11:23:15 +0100

    switched to terminus font

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Mono:pixelsize=13:antialias=true:autohint=true";
+static char font[] = "xos4 Terminus:pixelsize=15:antialias=true:autohint=true";
 static int borderpx = 15;
 
 /*
