2ad92bd908d1d491f7b3afcdb94e03d1a479f6ec
AxelBohm <axel.boehm@ymail.com>
2021-05-31 12:40:03 +0200

    terminus font name changed in arch linux

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char font[] = "xos4 Terminus:pixelsize=12";
+static char font[] = "Terminus:pixelsize=14";
 static int borderpx = 12;
 
 /*
